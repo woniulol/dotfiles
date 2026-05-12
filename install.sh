e097759564c4a2685e6affc326c1b19d63ad8ce4
jnwang <jnwangcn@gmail.com>
2026-05-12 11:48:07 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,75 @@
+cd /
+sudo apt update && sudo apt install -y \
+    curl \
+    net-tools \
+    apt-transport-https \
+    ca-certificates \
+    build-essential \
+    software-properties-common \
+    gnupg \
+    git \
+    stow \
+    vim \
+    gpg \
+    python3-venv
+
+# ssh
+sudo apt update && sudo apt install -y openssh-server
+systemctl enable ssh
+systemctl start ssh
+eval $(ssh-agent)
+
+mkdir -p "${HOME}/.local/bin/"
+mkdir -p "${HOME}/.config/"
+
+git clone https://github.com/woniulol/dotfiles.git
+cd dotfiles
+
+# zsh
+sudo apt install zsh
+chsh -s $(which zsh)
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+curl -LO https://github.com/junegunn/fzf/releases/download/v0.72.0/fzf-0.72.0-linux_amd64.tar.gz
+tar -xzf fzf-0.72.0-linux_amd64.tar.gz -C ~/.local/bin
+
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
+
+cd
+cd dotfiles
+stow zsh
+source .zshrc
+
+# nvim
+curl -LO https://github.com/neovim/neovim/releases/download/v0.12.2/nvim-linux-x86_64.tar.gz
+tar xzvf nvim-linux-x86_64.tar.gz
+sudo mv -v "${HOME}/nvim-linux-x86_64" /opt/nvim
+ln -s /opt/nvim/bin/nvim ~/.local/bin/nvim
+
+curl -LO https://github.com/tree-sitter/tree-sitter/releases/download/v0.26.8/tree-sitter-cli-linux-x64.zip
+unzip tree-sitter-cli-linux-x64.zip
+chmod +x tree-sitter
+sudo mv -v tree-sitter "${HOME}/.local/bin/"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 24
+node -v # Should print "v24.15.0".
+npm -v # Should print "11.12.1".
+
+cd
+cd dotfiles
+stow nvim-exp
+
+
+
+
+
+
+
--- nvim-exp/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim-exp/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -116,10 +116,26 @@ vim.lsp.config("ty", {
 	},
 })
 
+vim.lsp.config("ts_ls", {})
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+vim.lsp.config("html", {
+	capabilities = capabilities,
+})
+
+vim.lsp.config("cssls", {
+	capabilities = capabilities,
+})
+
 vim.lsp.enable({
 	"lua_ls",
 	"basedpyright",
 	"ruff",
 	"ty",
 	"bashls",
+	"ts_ls",
+	"html",
+	"cssls",
 })
